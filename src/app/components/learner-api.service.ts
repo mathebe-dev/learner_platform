362cfec662d61c7a9ffc35c408d2f95f62dc7c2c
mathebe <connymoseri0303@gmail.com>
2025-09-30 06:42:25 +0200

    deploy config

--- src/app/components/learner-api.service.ts
+++ src/app/components/learner-api.service.ts
@@ -1,23 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../enviroments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LearnerApiService {
 
-  private learnerApi = 'http://localhost:8000/get_learner';
-  private addlearnerApi = 'http://localhost:8000/add_learner';
+  private baseUrl = environment.apiUrl;
+
+  //private baseUrl = 'https://backend20250929151252-euckazbch2hwcte9.southafricanorth-01.azurewebsites.net/api/Learner_Platform';
 
   constructor(private http: HttpClient) {}
 
-  getLearner(): Observable<any>{
-    return this.http.get<any>(this.learnerApi);
+  getLearner(): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/get_learners`);
   }
 
   addLearner(learner: any): Observable<any> {
-    return this.http.post<any>(this.addlearnerApi, learner);
+    return this.http.post<any>(`${this.baseUrl}/add_learner`, learner);
   }
-
 }
---
+++ src/enviroments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://backend20250929151252-euckazbch2hwcte9.southafricanorth-01.azurewebsites.net/api/Learner_Platform'
+}
